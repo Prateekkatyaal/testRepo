a5e462893990edf7098d452e862df515c9a9da40
prateekkatyaal <pratikk.ic18@nsut.ac.in>
2021-05-06 21:47:21 +0530

    First Commit

---
+++ 2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+
+    cin>>a>>b;
+
+    cout<<"sid chutiya "<<a*b;
+
+    return 0;
+}
---
+++ 3.py
@@ -0,0 +1,6 @@
+num1=12
+num2= 14
+
+sum= num1+ nums2
+
+print(sum)
